0229fae17ad424b2d298ebc7899ed9b0fc4c2d94
codingbyfelix <felixow92@gmail.com>
2025-10-16 15:51:53 +0200

    Apply .gitignore clean

--- ha_tb.vhdl
+++ ha_tb.vhdl
@@ -20,10 +20,12 @@ begin
 
     process
     begin
+    	a <= 'X'; b <= 'X'; wait for 1 ns;
         a <= '0'; b <= '0'; wait for 1 ns;
         a <= '0'; b <= '1'; wait for 1 ns;
         a <= '1'; b <= '0'; wait for 1 ns;
         a <= '1'; b <= '1'; wait for 1 ns;
+        assert false report "Reached end of test";
         wait;
     end process;
 
