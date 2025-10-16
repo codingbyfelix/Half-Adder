cdf910dd5aad81c7cf8ce501c5fa7c89a0767078
codingbyfelix <felixow92@gmail.com>
2025-10-16 10:10:15 +0200

    Connecting wires to inputs

--- ha_tb.vhdl
+++ ha_tb.vhdl
@@ -2,4 +2,21 @@ library ieee;
 
 use ieee.std_logic_1164.all;
 
-entity ha_tb
+entity ha_tb is 
+end ha_tb;
+
+architecture test of ha_tb is
+    component ha;
+        port(
+            a: in std_ulogic;
+            b: in std_ulogic;
+            o: out std_ulogic;
+            c: out std_ulogic
+        );
+    end component; 
+
+    signal a,b,c,o : std_ulogic;
+begin
+        half_adder: ha port map (a => a, b => b, o => o, c => c);
+
+end test;
