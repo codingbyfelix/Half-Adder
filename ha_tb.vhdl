0e4cba09288dcc83cfdf0cafc8f2dfc87f6f9bc1
codingbyfelix <felixow92@gmail.com>
2025-10-16 10:16:15 +0200

    Created Process in ha_tb.vhdl

--- ha_tb.vhdl
+++ ha_tb.vhdl
@@ -19,4 +19,23 @@ architecture test of ha_tb is
 begin
         half_adder: ha port map (a => a, b => b, o => o, c => c);
 
+        process begin
+            a <= 'X';
+            b <= 'X';
+            wait for 1 ns;
+
+            a <= '0';
+            b <= '1';
+            wait for 1 ns;
+
+            a <= '1';
+            b <= '0';
+            wait for 1 ns;
+
+            a <= '1';
+            b <= '1';
+            wait for 1 ns;
+
+        end process;
+
 end test;
